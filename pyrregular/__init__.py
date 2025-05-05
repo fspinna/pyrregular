13ee5c79ccabdce3f107a7840a4ae573583b8b63
francesco <fspinna@gmail.com>
2025-05-05 19:28:13 +0200

    fix: update dataset loading function and add dataset listing utility

--- pyrregular/__init__.py
+++ pyrregular/__init__.py
@@ -1,1 +1,2 @@
-from pyrregular.repository import load_dataset_from_huggingface as load_dataset
+from pyrregular.repository import load_dataset_from_huggingface_via_xarray as load_dataset
+from pyrregular.data_utils import list_registry_datasets as list_datasets
