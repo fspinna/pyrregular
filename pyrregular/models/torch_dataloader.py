e4dd3c3a07de10a152d69db03b0689816ae55322
francesco <fspinna@gmail.com>
2025-07-27 23:52:58 +0200

    Refactoring

--- pyrregular/models/torch_dataloader.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch.utils.data import TensorDataset
-
-
-class IrregularDataset(TensorDataset):
-    def __init__(self, X, **kwargs):
-        super().__init__(torch.Tensor(X.irr.to_dense(**kwargs)[0]))
